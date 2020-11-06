1ba552a55931ae894f8cc6b6fea1a0d21892867d
Andrew Closs <andrew.closs2@ontario.ca>
2020-11-06 15:00:14 -0500

    Add more to example vue component.

--- examples/ExampleComponent.vue
+++ examples/ExampleComponent.vue
@@ -15,9 +15,58 @@
 </template>
 
 <script>
+import Filters from '../components/Filters.vue'
+import Pagination from '../components/Pagination.vue'
+import Features from '../components/Features.vue'
+
 export default {
-  mounted() {
-    console.log('Component mounted.')
-  }
-}
+
+  props: ['hasPagination', 'limit'],
+
+  components: {
+    Pagination,
+    Filters,
+    Features
+  },
+
+  data () {
+    return {
+      apiEndPoints: ['types', 'ministries', 'topics'],
+      results: [],
+      noResults: false,
+      page: 1,
+      lastPage: null,
+      total: null,
+      loading: false,
+      fetchingResults: false,
+      errored: false,
+      ministryName: null,
+      keywords: null,
+      filterValues: {
+        types: null,
+        ministry_acronym: null,
+        ministries: null,
+        topics: null,
+        start_date: null,
+        end_date: null,
+        sort: 'desc'
+      },
+      selected: []
+    }
+  },
+  computed: {
+    routeName () {
+      return this.$route.name
+    }
+  },
+  methods: {
+    // If query string data is present, set the keywords (if available)
+    setDataFromQueryString () {
+      let queryParams = this.$route.query
+      if ( Object.keys(queryParams).length !== 0 ) {
+        this.keywords = this.$route.query.keywords
+      }
+    },
+    // fetch the results data from the appropriate endpoint (releases/search)
+
 </script>
